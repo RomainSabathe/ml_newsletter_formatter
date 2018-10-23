ead54309cc3428f668a5835da1de55e061ccf9cc
Romain Sabathe <romain.sabathe@onfido.com>
2018-10-23 08:54:01 +0100

    Adding all the issues since... almost the beginning!

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import yaml
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
@@ -36,8 +38,14 @@ CATEGORIES_INFO = [
         'name': 'Data Science & Visualisations',
         'url_logo': 'https://image.flaticon.com/icons/png/512/680/680208.png'},
     {
+        'name': 'Production & Engineering',
+        'url_logo': 'https://imageog.flaticon.com/icons/png/512/248/248114.png'},
+    {
         'name': 'Everything Else',
         'url_logo': 'https://image.flaticon.com/icons/png/512/263/263051.png'},
+    {
+        'name': 'News',
+        'url_logo': 'https://image.flaticon.com/icons/png/512/125/125510.png'},
     {
         'name': 'Laugh of the week',
         'url_logo': 'https://image.flaticon.com/icons/png/512/56/56243.png'}]
@@ -54,27 +62,80 @@ TYPE_LOGOS = {
 
 
 
-def render():
+def render(yaml_path):
+    """Creates an HTML file corresponding to the newsletter created using
+    the yaml file at `yaml_path`.
+
+    Args:
+        yaml_path (str or pathlib.Path): path to the yaml file to be parsed.
+    """
     j2_env = Environment(loader=FileSystemLoader(TEMPLATE_DIR),
                          trim_blocks=True)
 
-    categories = _load_categories()
+    categories = _load_categories(yaml_path)
 
-    with open('rendered.html', 'w') as f:
+    html_path = yaml_path_to_html_path(yaml_path)
+    with html_path.open('w') as f:
         f.write(j2_env.get_template('template.html').render(categories=categories))
 
 
-def _load_categories():
-    with open('newsletters/yaml/week_005.yml', 'r') as f:
+def yaml_path_to_html_path(yaml_path):
+    """Converts a yaml filepath to the corresponding html filepath.
+    For instance: converts 'newsletters/yaml/week_032.yml' to:
+                           'newsletters/html/week_032.html'.
+
+    Args:
+        yaml_path (pathlib.Path): the yaml path to be converted.
+    """
+    parts = list(yaml_path.parts)
+    parts[1] = 'html'
+    return Path(*parts).with_suffix('.html')
+
+
+def clean_categories(category):
+    if category.lower() == 'nlp':
+        return 'Natural Language Processing'
+    elif category.lower() in ['drl', 'rl']:
+        return 'Reinforcement Learning'
+    elif category.lower() == 'cv':
+        return 'Computer Vision'
+    elif category.lower() in ['gan', 'gans']:
+        return 'GANs & Adversarial Attacks'
+    elif category.lower() in ['opti', 'optim', 'optimisation', 'optimization', 'theory']:
+        return 'Optimisation & Learning Theory'
+    elif category.lower() == 'dl':
+        return 'Deep Learning'
+    elif category.lower() in ['ml', 'general']:
+        return 'General Machine Learning'
+    elif category.lower() in ['inter', 'interpratibility', 'fair', 'fairness']:
+        return 'Interpratibility & Fairness'
+    elif category.lower() in ['ds', 'vis', 'visu', 'visualisations', 'viz']:
+        return 'Data Science & Visualisations'
+    elif category.lower() in ['engineering', 'prod']:
+        return 'Production & Engineering'
+    elif category.lower() == 'laugh':
+        return 'Laugh of the Week'
+    return 'Everything Else'
+
+
+def _load_categories(path):
+    """Parses a YAML file and returns a dictionary that should be used
+    by `template.html` to generate a newsletter page.
+
+    Args:
+        path (str or pathlib.Path): path to the yaml file to be parsed.
+    """
+    with path.open('r') as f:
         content = yaml.load(f)
 
     def add_article_logos(article):
         """Article is a dict."""
-        types = article['type'].split(' + ')
+        types = article['type'].split('+')
         article['urls_logo'] = []
         for type in types:
-            if type.lower() in TYPE_LOGOS:
-                article['urls_logo'].append(TYPE_LOGOS[type.lower()])
+            type = type.lower().strip()
+            if type in TYPE_LOGOS:
+                article['urls_logo'].append(TYPE_LOGOS[type])
 
         return article
 
@@ -83,6 +144,12 @@ def _load_categories():
 
     df = pd.DataFrame(content['articles'])
     df = df.fillna("")
+    df['category'] = df['category'].apply(clean_categories)
+    df['category'] = df['category'].str.title()
+
+    # Hack: placing the recommended articles on top.
+    df['recommended_helper'] = df.recommended.apply(lambda x: 0 if x else 1)
+    df = df.sort_values('recommended_helper')
 
     categories = []
     for category_name, group in df.groupby('category'):
@@ -114,4 +181,11 @@ def _get_category_info(category_name):
 
 
 if __name__ == '__main__':
-    render()
+    for yaml_path in Path('newsletters/yaml').iterdir():
+        # We don't generate if the HTML already exists.
+        html_path = yaml_path_to_html_path(yaml_path)
+        if not html_path.exists():
+            try:
+                render(yaml_path)
+            except Exception as e:
+                print(f'Couldnt generate {html_path}: {e}')
